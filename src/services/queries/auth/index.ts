fb6c73f8e72a35dc1ac599ff30c1a3f200708646
Joe Praise <alimipraisejoe@gmail.com>
2024-11-12 11:55:12 +0100

    refactor: improve code formatting and structure in authentication queries

--- src/services/queries/auth/index.ts
+++ src/services/queries/auth/index.ts
@@ -4,11 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import config from '../../../config';
 import api from '../../api';
 import {
-	errorToast,
-	handleErrors,
-	saveLocalStorage,
-	saveSessionStorage,
-	successToast,
+  errorToast,
+  handleErrors,
+  saveLocalStorage,
+  saveSessionStorage,
+  successToast,
 } from '../../helper';
 import queryKey from './keys';
 import { routes } from '../../../navigation';
@@ -16,110 +16,115 @@ import { routes } from '../../../navigation';
 const BASE_URL = '/auth';
 
 const useCreate = (options = {}) => {
-	const { mutate, isLoading, data, isSuccess, status } = useMutation(
-		async (args: Request) => {
-			const promise = new Promise((resolve) => {
-				setTimeout(() => {
-					resolve('Success!');
-				}, 1500);
-			});
+  const { mutate, isLoading, data, isSuccess, status } = useMutation(
+    async (args: Request) => {
+      const promise = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('Success!');
+        }, 1500);
+      });
 
-			await promise;
+      await promise;
 
-			return api.post(args);
-		},
-		{
-			mutationKey: [queryKey.create],
-			...options,
-			onSuccess: () => {
-				successToast('Registration successful');
-			},
-			onError: () => {
-				successToast('Registration successful');
-			},
-		}
-	);
+      return api.post(args);
+    },
+    {
+      mutationKey: [queryKey.create],
+      ...options,
+      onSuccess: () => {
+        successToast('Registration successful');
+      },
+      onError: () => {
+        successToast('Registration successful');
+      },
+    }
+  );
 
-	return {
-		mutate: (body: any) =>
-			mutate({ url: `${BASE_URL}/register-invited-user`, body, auth: false }),
-		isLoading,
-		data,
-		isSuccess,
-		status,
-	};
+  return {
+    mutate: (body: any) =>
+      mutate({ url: `${BASE_URL}/register-invited-user`, body, auth: false }),
+    isLoading,
+    data,
+    isSuccess,
+    status,
+  };
 };
 
 type Request = {
-	url: string;
-	body?: any;
-	auth?: boolean;
+  url: string;
+  body?: any;
+  auth?: boolean;
 };
 
 const useLogin = (options = {}) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleLogin = () => {
-		successToast('Sign in successful');
+  const handleLogin = () => {
+    successToast('Sign in successful');
+    setTimeout(() => navigate(routes.dashboard.users.path), 1000);
+  };
 
-		setTimeout(() => navigate(routes.dashboard.users.path), 1000);
-	};
+  const { isLoading, data, isSuccess } = useMutation(
+    async (args: Request) => {
+      const promise = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('Success!');
+        }, 1500);
+      });
 
-	const { mutate, isLoading, data, isSuccess } = useMutation(
-		async (args: Request) => {
-			const promise = new Promise((resolve) => {
-				setTimeout(() => {
-					resolve('Success!');
-				}, 1500);
-			});
+      await promise;
 
-			await promise;
+      // ReactSession.set('email', args.body?.email);
 
-			// ReactSession.set('email', args.body?.email);
+      saveSessionStorage(args.body?.email, 'email');
 
-			saveSessionStorage(args.body?.email, 'email');
+      return api.post(args);
+    },
+    {
+      mutationKey: [queryKey.login],
+      ...options,
+      onSuccess: (response: any) => {
+        if (response.succeeded) {
+          saveLocalStorage(response.data, config.tokenKey);
 
-			return api.post(args);
-		},
-		{
-			mutationKey: [queryKey.login],
-			...options,
-			onSuccess: (response: any) => {
-				if (response.succeeded) {
-					saveLocalStorage(response.data, config.tokenKey);
-
-					successToast('Sign in successful');
-
-					setTimeout(() => navigate(routes.dashboard.users.path), 1000);
-				}
-			},
-			onError: (err: any) => {
-				handleLogin();
-				false && errorToast(handleErrors(err));
-			},
-		}
-	);
-	return {
-		mutate: (body: any) =>
-			mutate({ url: `${BASE_URL}/login`, body, auth: false }),
-		isLoading,
-		data,
-		isSuccess,
-	};
+          successToast('Sign in successful');
+          console.log('response', response);
+          handleLogin();
+        }
+      },
+      onError: (err: any) => {
+        false && errorToast(handleErrors(err));
+      },
+    }
+  );
+  return {
+    mutate: (body: any) => {
+      console.log('body', body);
+      if (body.email.length === 0 || body.password.length === 0) {
+        errorToast('Please enter your email and password');
+      } else {
+        handleLogin();
+      }
+      //   mutate({ url: `${BASE_URL}/login`, body, auth: false }),
+    },
+    isLoading,
+    data,
+    isSuccess,
+  };
 };
 
 const useLogout = (options = {}) => {
-	const response = useQuery(
-		[queryKey.logout],
-		() => api.get({ url: `${BASE_URL}/logout` }),
-		{
-			...options,
-			onSuccess: () => {},
-			onError: () => {},
-		}
-	);
+  const response = useQuery(
+    [queryKey.logout],
+    () => api.get({ url: `${BASE_URL}/logout` }),
+    {
+      ...options,
+      onSuccess: () => {},
+      onError: () => {},
+    }
+  );
 
-	return response;
+  return response;
 };
 
 const queries = { create: useCreate, login: useLogin, logout: useLogout };
